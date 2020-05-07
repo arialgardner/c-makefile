e79476942110e44b2524135bf73bdba8bebc00b6
arialgardner <34754623+arialgardner@users.noreply.github.com>
2020-05-07 11:11:29 -0400

    Update myfunc.cpp

--- myfunc.cpp
+++ myfunc.cpp
@@ -1,14 +1,7 @@
-//#includes are ALWAYS at top of file
-#include <iostream>	//iostream is where std::cout and std::endl are defined
+#include "myfunc.h"
 
-std::string func()
+std::string myfunc()
 {
-	//say hello
+	//a different comment
 	return "hello";
-}
-
-int main()
-{
-	std::cout<<func()<<std::endl;
-	return 0;
 }
